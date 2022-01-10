f8ea57fc0717afba853900571f82048290ff0d2d
GuiCoelho-S <gs.coelho@outlook.com>
2022-01-10 13:35:56 -0300

    configuração primária

---
+++ styles/GlobalStyle.tsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+* {
+  padding: 0;
+  margin:0;
+  border:0;
+  box-sizing: border-box;
+  color:red;
+}
+
+
+
+body {
+  background-color: whitesmoke;
+}
+`
